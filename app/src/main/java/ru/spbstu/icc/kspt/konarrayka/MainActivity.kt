712efa48b92e7dc5feda16af1eac4deb1974f852
Sergei Vorobyov <hacker.mad.cat@gmail.com>
2018-09-29 18:06:07 +0300

    show narration log to phone window

--- app/src/main/java/ru/spbstu/icc/kspt/konarrayka/MainActivity.kt
+++ app/src/main/java/ru/spbstu/icc/kspt/konarrayka/MainActivity.kt
@@ -2,23 +2,32 @@ package ru.spbstu.icc.kspt.konarrayka
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import ru.spbstu.icc.kspt.configuration.ConfigurationManagerImpl
 import ru.spbstu.icc.kspt.storage.StorageManagerImpl
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val logView = findViewById<TextView>(R.id.logView)
+        logView.text = generateText()
+    }
+
+    private fun generateText() = StringBuilder().run {
+        appendln("hello world!")
         val configurationManager = ConfigurationManagerImpl()
         val storageManager = StorageManagerImpl()
         val file = storageManager.findFile()
         val model = configurationManager.loadModel(file)
         val scenario = configurationManager.getScenario(model)
         for (action in scenario.getActions()) {
-            println("-----------------")
-            println(action.getRoles().joinToString(" and ") { it.getName() })
-            println(action.getText())
+            appendln("-----------------")
+            appendln(action.getRoles().joinToString(" and ") { it.getName() })
+            appendln(action.getText())
         }
+        toString()
     }
 }
