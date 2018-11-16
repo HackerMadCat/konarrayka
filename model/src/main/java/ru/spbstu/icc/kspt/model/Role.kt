bdb9b31e861cb7e5889900324d14b3f586d23e0e
Sergei <hacker.mad.cat@gmail.com>
2018-11-16 12:35:37 +0300

    remove redundant model module

--- model/src/main/java/ru/spbstu/icc/kspt/model/Role.kt
+++
@@ -1,8 +0,0 @@
-package ru.spbstu.icc.kspt.model
-
-
-data class Role(private val name: String) {
-    fun getName(): String {
-        return name
-    }
-}
--- model/src/main/java/ru/spbstu/icc/kspt/model/Scenario.kt
+++
@@ -1,8 +0,0 @@
-package ru.spbstu.icc.kspt.model
-
-class Scenario(private val actions: List<Action>) {
-
-    fun getActions(): List<Action> {
-        return actions
-    }
-}
