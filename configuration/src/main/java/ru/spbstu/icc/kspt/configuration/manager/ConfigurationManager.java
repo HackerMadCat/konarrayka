3258b5f3c45814faa8ff5e0853a8967a9a3d9ad6
LazarevGleb <lazarevgb@gmail.com>
2018-11-18 03:00:49 +0300

    Scan Model to find icons and sound files

--- configuration/src/main/java/ru/spbstu/icc/kspt/configuration/manager/ConfigurationManager.java
+++ configuration/src/main/java/ru/spbstu/icc/kspt/configuration/manager/ConfigurationManager.java
@@ -7,12 +7,20 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+import ru.spbstu.icc.kspt.configuration.model.Action;
+import ru.spbstu.icc.kspt.configuration.model.ConditionalAction;
+import ru.spbstu.icc.kspt.configuration.model.Hero;
 import ru.spbstu.icc.kspt.configuration.model.Model;
+import ru.spbstu.icc.kspt.configuration.model.Setup;
 
 public class ConfigurationManager {
     private Context context;
@@ -24,14 +32,76 @@ public class ConfigurationManager {
     public void writeToInternalMemory(Model model) {
         Gson gson = new GsonBuilder().create();
         String json = gson.toJson(model);
+
+        Set<File> sounds = populateSounds(model);
+        Set<File> icons = populateIcons(model);
+
         try (OutputStreamWriter osw = new OutputStreamWriter(
-                context.openFileOutput("config_" + model.getId() + ".txt",
+                context.openFileOutput("config_" + model.getHeader().getId() + ".txt",
                         Context.MODE_PRIVATE))) {
             osw.append(json);
         } catch (IOException e) {
             Log.v("ERROR", "Writing error", e);
         }
     }
+
+    private Set<File> populateSounds(Model model) {
+        Set<File> sounds = new HashSet<>(model.getSounds());
+
+        if (model.getRules() != null) {
+            Set<Action> actions = model.getRules().getActions();
+            for (Action action : actions) {
+                sounds.addAll(action.getVoices());
+            }
+
+            List<ConditionalAction> conditionalActions = model.getRules().getConditionalActions();
+            for (ConditionalAction ca : conditionalActions) {
+                if (ca.getAction() != null) {
+                    sounds.addAll(ca.getAction().getVoices());
+                }
+            }
+        }
+        return sounds;
+    }
+
+    private Set<File> populateIcons(Model model) {
+        Set<File> icons = new HashSet<>();
+        if (model.getHeader() != null) {
+            icons.add(model.getHeader().getIcon());
+        }
+
+        if (model.getRules() != null) {
+            for (Hero hero : model.getRules().getHeroes()) {
+                icons.add(hero.getIcon());
+            }
+
+            for (Action action : model.getRules().getActions()) {
+                icons.add(action.getIcon());
+            }
+
+            for (ConditionalAction ca : model.getRules().getConditionalActions()) {
+                if (ca.getAction() != null) {
+                    icons.add(ca.getAction().getIcon());
+                }
+                if (ca.getCondition() != null) {
+                    for (Set<Hero> setHeroes : ca.getCondition().getDisjunctions()) {
+                        for (Hero hero : setHeroes) {
+                            icons.add(hero.getIcon());
+                        }
+                    }
+                }
+            }
+        }
+
+        for (Setup setup : model.getSetups()) {
+            for (Hero hero : setup.getHeroes()) {
+                icons.add(hero.getIcon());
+            }
+        }
+
+        return icons;
+    }
+
 
     public Model getModelById(int uuid) {
         try (InputStream is = context.openFileInput("config_" + uuid + ".txt")) {
