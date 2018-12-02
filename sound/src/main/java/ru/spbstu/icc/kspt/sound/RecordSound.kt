f8fd6ab3cde233d333f4846f64338373570435ab
Alexandr Nadraga <nadraga.alexandr@yandex.ru>
2018-12-02 20:23:09 +0300

    Some changes in RecordSound activity

--- sound/src/main/java/ru/spbstu/icc/kspt/sound/RecordSound.kt
+++ sound/src/main/java/ru/spbstu/icc/kspt/sound/RecordSound.kt
@@ -1,28 +1,33 @@
 package ru.spbstu.icc.kspt.sound
 
 import android.Manifest
+import android.app.Activity
 import android.content.pm.PackageManager
 import android.media.MediaPlayer
 import android.media.MediaRecorder
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
+import android.support.v7.app.ActionBar
 import android.util.Log
+import android.view.MenuItem
 import android.view.View
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_record_sound.*
 
 import java.io.File
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.concurrent.timerTask
 
 class RecordSound : AppCompatActivity() {
-    private var filename: String? = null
-    private var mediaRecorder: MediaRecorder? = null
-    private var mediaPlayer: MediaPlayer? = null
+    private lateinit var mediaRecorder: MediaRecorder
+    private lateinit var mediaPlayer: MediaPlayer
     private var permissionToRecord = false
     private var permissions: Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
     private lateinit var file:File
+    private lateinit var timer:Timer
 
     //request for RecordPermission
     override fun onRequestPermissionsResult(
@@ -41,49 +46,72 @@ class RecordSound : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_record_sound)
-//        filename = "${externalCacheDir.absolutePath}/${FileName.text}.3gp"
-//        file = File(this.filesDir,FileName.text.toString())
-//        filename = file?.path
+        val actionBar:ActionBar? = supportActionBar
+        actionBar?.setHomeButtonEnabled(true)
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+        stop_button.isEnabled = false
         ActivityCompat.requestPermissions(this,permissions, REQUEST_RECORD_AUDIO_PERMISSION)
-        textView3.text = filename
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            android.R.id.home -> {
+                if (!::file.isInitialized){
+                    this.finish()
+                    return true
+                }
+                intent.putExtra("RECORDED_SOUND",file.path)
+                setResult(Activity.RESULT_OK,intent)
+                this.finish()
+                return true
+            }
+            else-> return super.onOptionsItemSelected(item)
+        }
     }
 
     //Create Filename by date
     private fun createFileName():String{
         val timestamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        return "REC_${timestamp}"
+        DurationText.text =  "Sound${timestamp}"
+        return "Sound${timestamp}"
     }
 
     //release the media recorder, set record param and start record
     fun recordStart(v:View) {
-//        filename = "${externalCacheDir.absolutePath}/${FileName.text}.3gp"
-        file = File(filesDir,FileNameField.text.toString())
-        val outFile = File(this.filesDir, FileNameField.text.toString())
-        if (outFile.exists()) {
-            outFile.delete()
+        if (editText.text.isEmpty()) {
+            DurationText.text = "FieldIsEmpty"
+            Log.e("EditableTest","EditableIsEmpty================================")
+            return
         }
+        if (!::file.isInitialized) {file = File(this.filesDir,editText.text.toString()+".3gp")}
         mediaRecorder = MediaRecorder().apply {
             setAudioSource(MediaRecorder.AudioSource.MIC)
             setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
-            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
-            setOutputFile(outFile.path)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_WB)
+            setOutputFile(file.path)
+//            setAudioSamplingRate(44100)
+//            setAudioEncodingBitRate()
+            DurationText.text = file.nameWithoutExtension
             try {
                 prepare()
             } catch (e: IOException){
                 Log.e("RecordTest","<====prepare() failed=====>")
             }
             start()
-            button3.isEnabled = false
         }
+        rec_button.isEnabled = false
+        stop_button.isEnabled = true
+        changeButtonTaskAndDrawable(stop_button,"stop","recStop")
     }
 
     //release recorder and stop recording
     fun recordStop (v:View){
-        mediaRecorder?.apply {
+        mediaRecorder.apply {
             stop()
             release()
         }
-        button3.isEnabled = true
+        rec_button.isEnabled = true
+        changeButtonTaskAndDrawable(stop_button,"play","play")
     }
 
     //set path to the file to listening and start playing
@@ -98,16 +126,28 @@ class RecordSound : AppCompatActivity() {
                 Log.e("RecordTest","<====prepare() failed ====>")
             }
         }
-        textView3.text = file.path
+        timer = Timer()
+        timer.schedule( timerTask { changeButtonTaskAndDrawable(stop_button,"play","play") },
+                mediaPlayer.duration.toLong())
+        changeButtonTaskAndDrawable(stop_button,"stop","stop")
     }
 
     //stop playing file
     fun playStop (v:View){
-        mediaPlayer?.release()
-        mediaPlayer = null
+        mediaPlayer.release()
+        timer.cancel()
+        changeButtonTaskAndDrawable(stop_button,"play","play")
     }
 
-
+    fun changeButtonTaskAndDrawable(button:ImageView,drawable:String,task:String) {
+        button.setImageResource(resources.getIdentifier(drawable, "drawable", packageName))
+        when (task) {
+            "play" -> button.setOnClickListener { playStart(button) }
+            "stop" -> button.setOnClickListener { playStop(button) }
+            "recStop" -> button.setOnClickListener { recordStop(button) }
+            else -> return
+        }
+    }
     companion object {
         //permission code
         private const val REQUEST_RECORD_AUDIO_PERMISSION = 200
