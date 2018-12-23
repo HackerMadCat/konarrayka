14d09b5148d917b8c8673435a8349ec1a7655332
Alexandr Nadraga <nadraga.alexandr@yandex.ru>
2018-12-23 21:12:40 +0300

    Some changes in Hero model

--- configuration/src/main/java/ru/spbstu/icc/kspt/configuration/model/Hero.kt
+++ configuration/src/main/java/ru/spbstu/icc/kspt/configuration/model/Hero.kt
@@ -1,12 +1,13 @@
 package ru.spbstu.icc.kspt.configuration.model
 
 import java.io.File
+import java.io.Serializable
 
 data class Hero(
         val icon: File,
         val name: String,
         val color: Int
-) {
+):Serializable {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is Hero) return false
