95bf59ef42f77abfe762ee491fde7c676a2754f2
Alexandr Nadraga <nadraga.alexandr@yandex.ru>
2018-12-25 03:35:58 +0300

    some changes in Action Model

--- configuration/src/main/java/ru/spbstu/icc/kspt/configuration/model/Action.kt
+++ configuration/src/main/java/ru/spbstu/icc/kspt/configuration/model/Action.kt
@@ -1,14 +1,17 @@
 package ru.spbstu.icc.kspt.configuration.model
 
 import java.io.File
+import java.io.Serializable
 
 data class Action(
         val icon: File,
         val name: String,
         val description: String,
         val voices: List<File>,
-        val duration: Duration
-) {
+//         coz ya ne uspel sdelat' shtyky, kotoraya delaet Duration peremennie
+//         val duration: Duration
+        val duration: Int
+):Serializable {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is Action) return false
